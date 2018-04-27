771443eb07debf49960cd6012eaf503ed7c2ae65
Atul Dislay <atuldislay@gmail.com>
2018-04-27 15:45:55 +0530

    Add files via upload

---
+++ CreateUserCode/CreateUserCode/ManageUser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.DirectoryServices;
+
+
+
+namespace CreateUserCode
+{
+    class ManageUser
+    {
+        public static void CreateUser(string passedName, string pass)
+        {
+            try
+            {
+                DirectoryEntry ad = new DirectoryEntry("WinNT://" + Environment.MachineName + ",computer");
+                DirectoryEntry newUser = ad.Children.Add(passedName, "user");
+                newUser.Invoke("SetPassword", new object[] { pass });
+                newUser.Invoke("Put", new object[] { "Description", "Test User from .NET" });
+                newUser.CommitChanges();
+                DirectoryEntry grp;
+
+                grp = ad.Children.Find("Administrators", "group");
+                if (grp != null) { grp.Invoke("Add", new object[] { newUser.Path }); }
+                Console.WriteLine("Account Created Successfully");
+                Console.WriteLine("Press Enter to continue....");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+
+            }
+
+        }
+
+        public static void DeleteUser(string passedName)
+        {
+            DirectoryEntry localDirectory = new DirectoryEntry("WinNT://" + Environment.MachineName);
+            DirectoryEntries users = localDirectory.Children;
+            DirectoryEntry user = users.Find(passedName);
+            users.Remove(user);
+        }
+
+       public static void ChangePassword(string passedName, string pass)
+        {
+            DirectoryEntry localDirectory = new DirectoryEntry("WinNT://" + Environment.MachineName);
+            DirectoryEntries users = localDirectory.Children;
+            DirectoryEntry user = users.Find(passedName);
+            user.Password = pass;
+        }
+    }
+}
---
+++ CreateUserCode/CreateUserCode/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.DirectoryServices;
+
+
+namespace CreateUserCode
+{
+    class Program
+    {
+        public static string Name;
+        public static string Pass;
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Windows Account Creator");
+            //Console.WriteLine("Enter User passedName");
+            //Name = Console.ReadLine();
+            //Console.WriteLine("Enter User Password");
+            //Pass = Console.ReadLine();
+            //ManageUser.CreateUser(Name, Pass);
+            ManageUser.ChangePassword("rahulG",@"@@@qwe123");
+        }
+    }  
+}
